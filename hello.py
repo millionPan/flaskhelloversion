3acd212e7b828c7b6adf1cc48ba0e8873f9d65df
millionPan <809251311@qq.com>
2019-04-03 09:22:52 +0800

    v2-动态路由

--- hello.py
+++ hello.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/a')
 def hello():
     return '<h1 style="color:purple;font-size:40px;">hello,World!</h1>'
+
+@app.route('/hello/')
+def hello2():
+    return 'hello,Worldtoo!'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>hello,%s!</h1>' % name
 
 
 if __name__=='__main__':
