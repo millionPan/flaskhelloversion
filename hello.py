461eb9e3038d7bdb08968ac7cf694b8d499eed17
millionPan <809251311@qq.com>
2019-04-06 10:32:31 +0800

    v5-普通方式处理表单

--- hello.py
+++ hello.py
@@ -1,9 +1,33 @@
 from flask import Flask,render_template
+from flask import request
 
 app=Flask(__name__)
-
-@app.route('/')
+# 目的：实现一个简单的登录的逻辑处理
+# 1.路由需要有get和post两种请求方式  -->需要判断请求方式
+# 2.获取请求的参数
+# 3.判断参数是否填写&密码是否相同
+# 4.如果判断都没有问题，就返回一个success
+@app.route('/',methods=['GET','POST'])
 def hello():
+    #request:请求对象--> 获取请求方式、数据
+
+    # 1.判断请求方式
+    if request.method=='POST':
+
+        # 2.获取请求的参数
+        username=request.form.get('username')
+        password=request.form.get('password')
+        password2=request.form.get('password2')
+        print(password)
+
+        # 3.判断参数是否填写&密码是否相同
+        if not all([username,password,password2]):
+            print('参数不完整')
+        elif password !=password2:
+            print('密码不一致')
+        else :
+            return 'success'
+        # fruits=['apple','pear','grapes']
     return render_template('index.html')
 
 @app.route('/hello/')
